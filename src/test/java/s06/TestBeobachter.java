d7b074409eb2388e443d1e9270448578ffb31974
tgm-sgeyer <sgeyer@student.tgm.ac.at>
2014-12-17 08:47:10 +0100

    Added Testcases

---
+++ src/test/java/s06/TestBeobachter.java
@@ -0,0 +1,15 @@
+package s06;
+
+import s06.Beobachter;
+import s06.QuakBeobachtungsSubjekt;
+
+/**
+ * Created by Stefan on 17.12.2014.
+ */
+public class TestBeobachter implements Beobachter {
+
+    @Override
+    public void aktualisieren(QuakBeobachtungsSubjekt ente) {
+
+    }
+}
---
+++ src/test/java/s06/TestMoorEnte.java
@@ -0,0 +1,44 @@
+package s06;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Stefan on 17.12.2014.
+ */
+public class TestMoorEnte {
+
+    private ByteArrayOutputStream sysOut;
+
+    @Before
+    public void before() {
+        this.sysOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(sysOut));
+    }
+
+    @Test
+    public void testRegistriereBeobachter() {
+        MoorEnte me = new MoorEnte();
+        me.registriereBeobachter(new TestBeobachter());
+        //assertEquals(1, me.senderRing.getBeobachtende().);
+    }
+
+    @Test
+    public void testToString() {
+        MoorEnte me = new MoorEnte();
+        assertEquals("Moorente", me.toString());
+    }
+
+    @After
+    public void after() throws IOException {
+        this.sysOut.flush();
+        this.sysOut.close();
+    }
+}
